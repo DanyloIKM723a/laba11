6dbe3794b4e57ca3145d48631913b68a009c86a0
Danylo Masiyk <163041483+DanyloIKM723a@users.noreply.github.com>
2024-05-10 09:39:17 +0300

    Add files via upload

---
+++ laba11.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int* ptr;
+
+
+    ptr = new int;
+
+
+    cout << "Enter a value: ";
+    cin >> *ptr;
+
+
+    *ptr += 2;
+
+
+    cout << "Updated value: " << *ptr << endl;
+
+
+    delete ptr;
+
+    return 0;
+}
