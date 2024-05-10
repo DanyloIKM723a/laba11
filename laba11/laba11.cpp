53b031a8d575def43db1301e01d405e300a1f269
grush <grush@DESKTOP-P5A2MM5>
2024-05-10 09:30:28 +0300

    Добавьте файлы проекта.

---
+++ laba11/laba11.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double a, b;
+
+
+    cout << "Enter the value of a: ";
+    cin >> a;
+    cout << "Enter the value of b: ";
+    cin >> b;
+
+
+    double* ptrA = &a;
+    double* ptrB = &b;
+
+
+    *ptrA *= 3;
+    *ptrB /= 3;
+
+
+    cout << "Updated value of a: " << a << endl;
+    cout << "Updated value of b: " << b << endl;
+
+    return 0;
+}
